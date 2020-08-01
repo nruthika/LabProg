cc4414b7dbe0e9d25c6c27393268f540e85dd503
Darlo <Darlo@192.168.2.24>
2020-08-01 16:29:30 +0530

    commit

---
+++ trainee-springboot-appusingcollections/src/main/java/com/capg/traineespringbootusingcollections/TraineeSpringbootAppusingcollectionsApplication.java
@@ -0,0 +1,13 @@
+package com.capg.traineespringbootusingcollections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraineeSpringbootAppusingcollectionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraineeSpringbootAppusingcollectionsApplication.class, args);
+	}
+
+}
